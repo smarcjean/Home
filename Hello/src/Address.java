255e8a17a9296cf7cc82656cf6e3f6ba57689a3d
smarcjean <43018324+smarcjean@users.noreply.github.com>
2018-11-25 22:12:38 -0500

    In working progress

---
+++ Hello/src/Address.java
@@ -0,0 +1,34 @@
+
+public class Address {
+	private String street;
+	private String cityName;
+	private String stateName;
+	private String zipcode;
+
+	public String setStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCityName() {
+		return cityName;
+	}
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+	public String getStateName() {
+		return stateName;
+	}
+	public void setStateName(String stateName) {
+		this.stateName = stateName;
+	}
+	public String getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	
+	
+}
--- Hello/src/Draw.java
+++ Hello/src/Draw.java
@@ -1,19 +1,69 @@
 
 public class Draw {
 public static void main(String[] args) {
-	drawBox();
-	System.out.println();
-	drawBox();
-	drawBox();
-}
-
-
-public static void drawBox() {
-	System.out.println("+-----+");
-	System.out.println("|     | ");
-	System.out.println("|     | ");
-	System.out.println("+-----+");
 	
+	// ++a means add one then assign
+	// a++ means assign and then add one
+	// same for --a and a--
+String d;
+int a, b, c;
+a = 7; b = 8; c = 4;
+System.out.println(a *c);
+System.out.println("b / c = " + b / c);
+System.out.println("a = " + ++a);
+System.out.println("a = " + a++);
+System.out.println("a = " + a);
+System.out.println("a = " + --a);
+
+d = a == c ? "yes" : "no";
+System.out.println(d);
+
+System.out.println(Character.isLetter('g')); // true
+System.out.println(Character.isLetter('6')); // false
+System.out.println(Character.isDigit('8')); // true
+System.out.println(Character.isWhitespace('\t')); // true
+System.out.println(Character.isUpperCase('k')); // false
+System.out.println(Character.isLowerCase('G')); // false
+System.out.println(Character.toUpperCase('d')); // C
+System.out.println(Character.toLowerCase('K')); // c
+System.out.println(Character.toString('S')); // C
+
+
+
+		// This is an array
+		//datatype[] identifier; this the format
+		int[] age = new int[5];
+		//By using new operator array can be initialized.
+
+		//int[] age = new int[5];    //5 is the size of array.
+
+		//Arrays can be initialized at declaration time also.
+
+		//int age[5]={22,25,30,32,35};
+		
+		//Initializing each element separately in the loop.
+
+		        int[] newArray = new int[5];
+
+		        // Initializing elements of array seperately
+		        for (int n = 0; n < newArray.length; n++) {
+		            newArray[n] = n;
+		        }
+            
+		// this is an if statement
+    //  int a=20, b=30;
+
+   if(b>a) 
+   System.out.println("b is greater");
+   
+   // If-else statements
+  // int a = 80, b = 30;
+
+  // if (b & gt; a) {
+    System.out.println("b is greater");
+  // } else {
+    System.out.println("a is greater");
+   
 	
 }
 }
